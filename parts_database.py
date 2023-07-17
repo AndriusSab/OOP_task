969976cf9edf7ae96af079723631ef7dd732d3be
Andrius Sabaliauskas <andriussa@print-it.lt>
2023-07-17 20:24:03 +0300

    adding CPU and Cooler parts

--- parts_database.py
+++ parts_database.py
@@ -14,19 +14,39 @@ parts_database = {
             "speed": "3.8 GHz",
             "power_usage": 125
         },
-        "cpu3" : {            
+        "cpu3": {            
             "name": "Intel Core i9-13900K",
             "price": 569.99,
             "brand": "Intel",
             "speed": "5.8 GHz",
             "power_usage": 62
-            
         }, 
-        "cpu4" : {           
+        "cpu4": {           
             "name": "AMD Threadripper 3970X",
             "price": 2300.98,
             "brand": "AMD",
             "speed": "4.3 GHz",
-            "power_usage": 65}
+            "power_usage": 65
+        }
+    },
+    "cpu_cooler": {
+        "cooler1": {
+            "name": "Cooler Master Hyper 212 RGB",
+            "price": 49.99,
+            "cooler_type": "Air Cooler",
+            "noise_level": 26.0
+        },
+        "cooler2": {
+            "name": "NZXT Kraken X63",
+            "price": 149.99,
+            "cooler_type": "Liquid Cooler",
+            "noise_level": 36.0
+        },
+        "cooler3": {
+            "name": "Noctua NH-D15",
+            "price": 99.95,
+            "cooler_type": "Air Cooler",
+            "noise_level": 24.6
+        }
     }
 }
--- phase_one.py
+++ phase_one.py
@@ -7,28 +7,28 @@
 
 
 """PC PART PICKER """
-from  parts_database import parts_database
+from parts_database import parts_database
 from typing import Dict
 from abc import ABC, abstractmethod
 
 class PCPARTS(ABC):
-    def __init__(self, name:str, price: float):
+    def __init__(self, name: str, price: float):
         self.name = name
         self.price = price
     
     def get_name(self) -> str:
         return self.name
     
-    def  get_price(self) -> float:
+    def get_price(self) -> float:
         return self.price
     
     @abstractmethod
-    def print_smth(self):
+    def print_additional_info(self):
         pass
     
 
 class CPU(PCPARTS):
-    def __init__(self, name: str, price:float, brand:str, speed:float, power_usage:int):
+    def __init__(self, name: str, price: float, brand: str, speed: float, power_usage: int):
         super().__init__(name, price)
         self.brand = brand
         self.speed = speed
@@ -39,31 +39,66 @@ class CPU(PCPARTS):
         cpu_items = parts_database.get("cpu", {})
         cpu_objects = []
         for item_key, item_data in cpu_items.items():
-            cpu = CPU(item_data["name"], item_data["price"], item_data["brand"], item_data["speed"], item_data["power_usage"])
+            cpu = CPU(
+                item_data["name"], item_data["price"], item_data["brand"],
+                item_data["speed"], item_data["power_usage"]
+            )
             cpu_objects.append(cpu)
-        return cpu_objects       
- 
-    def get_brand(self):
+        return cpu_objects
+    
+    def get_brand(self) -> str:
         return self.brand
     
-    def get_speed(self):
+    def get_speed(self) -> float:
         return self.speed
     
-    def get_power_usage(self):
+    def get_power_usage(self) -> int:
         return self.power_usage
     
-    
-    def print_smth(self):
+    def print_additional_info(self):
         return f"CPU Information"
     
-    
-cpus = CPU.get_all_cpus()
 
+class CPUCooler(PCPARTS):
+    def __init__(self, name: str, price: float, cooler_type: str, noise_level: float):
+        super().__init__(name, price)
+        self.cooler_type = cooler_type
+        self.noise_level = noise_level
+        
+    @staticmethod
+    def get_all_cpu_coolers():
+        cpu_cooler_items = parts_database.get("cpu_cooler", {})
+        cpu_cooler_objects = []
+        for item_key, item_data in cpu_cooler_items.items():
+            cpu_cooler = CPUCooler(
+                item_data["name"], item_data["price"], item_data["cooler_type"],
+                item_data["noise_level"]
+            )
+            cpu_cooler_objects.append(cpu_cooler)
+        return cpu_cooler_objects
+
+    def print_additional_info(self):
+        return f"CPUCooler Information"
+
+
+cpus = CPU.get_all_cpus()
+print(cpus)
 
 for cpu in cpus:
+    print("\n----CPU info-----\n")
     print("Name:", cpu.get_name())
     print("Price:", cpu.get_price())
     print("Brand:", cpu.get_brand())
     print("Speed:", cpu.get_speed())
     print("Power Usage:", cpu.get_power_usage())
-    print("\n----CPU info-----")
+
+
+cpu_coolers = CPUCooler.get_all_cpu_coolers()
+print(cpu_coolers)
+
+for cpu_cooler in cpu_coolers:
+    print("\n----CPU Cooler info-----\n")
+    print("Name:", cpu_cooler.get_name())
+    print("Price:", cpu_cooler.get_price())
+    print("Cooler Type:", cpu_cooler.cooler_type)
+    print("Noise Level:", cpu_cooler.noise_level)
